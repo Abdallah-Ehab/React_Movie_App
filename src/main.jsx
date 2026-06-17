b12d98da6df6492fe095780a1adeb831551bbf8d
Alaa Anwar <anwaralaa286@gmail.com>
2026-06-17 08:24:00 +0300

    feat: integrate AuthProvider into application root

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App'
+import { AuthProvider } from './context/AuthContext'
 import { WishlistProvider } from './context/WishlistContext'
 import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <WishlistProvider>
-        <App />
-      </WishlistProvider>
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <AuthProvider>
+          <WishlistProvider>
+            <App />
+          </WishlistProvider>
+        </AuthProvider>
+      </ClerkProvider>
     </ThemeProvider>
   </StrictMode>,
 )
